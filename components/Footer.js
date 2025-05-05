3ae1752ffaa471b0ff98677e60dbdafd222098cd
Daegyu Han <bigstar77han@gmail.com>
2025-05-05 14:39:15 +0900

    [FIX] AdSense 광고 삽입 테스트 (html 파일 위치 수정) - tv0.0.4

--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
                     © {new Date().getFullYear()} Piggy AI. All rights reserved.
                 </p>
                 <p className="text-sm mt-2">
-                    t-v0.0.3
+                    t-v0.0.4
                 </p>
             </div>
         </div>
