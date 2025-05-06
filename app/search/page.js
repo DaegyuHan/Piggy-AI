ebf69a86c04340548238e651ab216b4a0e9b10a4
Daegyu Han <bigstar77han@gmail.com>
2025-05-07 03:12:58 +0900

    [FIX] /search useSearchParams() Error Fix - tv0.2.3

--- app/search/page.js
+++ app/search/page.js
@@ -4,19 +4,21 @@ import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 import SearchInput from "@/components/SearchInput";
 import CafeListWithResults from "@/components/CafeListWithResults";
-import PopularSearches from "@/components/PopularSearches";
 
-export default function SearchPage() {
+function SearchPageContent() {
     const searchParams = useSearchParams();
     const query = searchParams.get("query");
 
+    return <CafeListWithResults query={query} />;
+}
+
+export default function SearchPage() {
     return (
         <div className="flex flex-col items-center justify-start min-h-screen bg-gray-100">
             <SearchInput />
-            <Suspense fallback={<div>로딩 중...</div>}>
-                <CafeListWithResults query={query} />
+            <Suspense fallback={<div>검색 결과 불러오는 중...</div>}>
+                <SearchPageContent />
             </Suspense>
-            <PopularSearches/>
         </div>
     );
 }
