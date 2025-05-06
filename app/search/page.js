30289756c58ee1a5b429d1050039f629a68890a8
DaegyuHan <bigstarhan33@naver.com>
2025-05-06 17:59:55 +0900

    [FIX] Suspense Error fix - tv0.1.5

--- app/search/page.js
+++ app/search/page.js
@@ -1,11 +1,14 @@
+import { Suspense } from 'react'; // Suspense import 추가
 import SearchPageWithResults from "@/app/search/SearchPageWithResults";
 import SearchInput from "@/components/SearchInput";
 
 export default function Page() {
     return (
         <div className="flex flex-col items-center justify-start min-h-screen bg-gray-100">
-            <SearchInput/>
-            <SearchPageWithResults/>
+            <SearchInput />
+            <Suspense fallback={<div>로딩 중...</div>}>
+                <SearchPageWithResults />
+            </Suspense>
         </div>
     );
 }
--- components/Footer.js
+++ components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
                     © {new Date().getFullYear()} Piggy AI. All rights reserved.
                 </p>
                 <p className="text-sm mt-2">
-                    tv0.1.4
+                    tv0.1.5
                 </p>
             </div>
         </div>
